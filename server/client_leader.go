a277c55ade3542b88fdea5ea8562a0b53bbd8a05
seanlee0923 <leesinue0923@gmail.com>
2025-05-28 17:25:31 +0900

    정렬 반대로 된거 수정

--- server/client_leader.go
+++ server/client_leader.go
@@ -4,24 +4,30 @@ import (
 	"database/sql"
 	"errors"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"pls/mysql"
 )
+
+type LeaderRequest struct {
+	DeployName string `json:"deploy_name"`
+	ClientId   string `json:"client_id"`
+}
 
 // GenLeader 함수는 요청한 디플로이먼트 중 누가 리더인지 선택 후
 // 요청자에게 클라이언트 아이디를 응답합니다.
 // 가장 최근에 등록된 파드를 리더로 선택합니다.
 func GenLeader(c *gin.Context) {
 
-	deployName := c.Query("deployment")
-	if deployName == "" {
+	var req LeaderRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "deployment name required"})
 		return
 	}
 
 	var leaderId string
 
-	leaderRow := mysql.Db.QueryRow(`SELECT id FROM pls_clients WHERE deploy_name = ? ORDER BY created_at DESC LIMIT 1`, deployName)
+	leaderRow := mysql.Db.QueryRow(`SELECT id FROM pls_clients WHERE deployment_name = ? ORDER BY created_at ASC LIMIT 1`, req.DeployName)
 	if err := leaderRow.Scan(&leaderId); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "deployment not found"})
@@ -30,7 +36,8 @@ func GenLeader(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "db err"})
 		return
 	}
+	log.Println(leaderId)
 
-	c.JSON(http.StatusOK, gin.H{"leader_id": leaderId})
+	c.JSON(http.StatusOK, gin.H{"client_id": leaderId})
 
 }
